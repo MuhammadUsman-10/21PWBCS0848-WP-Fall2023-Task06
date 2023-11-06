8f6b853c6cf6cd228050a0c38f66d103cd96ee62
Muhammad Usman <usman.aamir404@gmail.com>
2023-11-06 23:24:19 +0500

    message

---
+++ script.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bookList = document.getElementById("book-list");
+    const addBookForm = document.getElementById("add-book-form");
+    const searchForm = document.getElementById("search-books");
+    const searchInput = document.getElementById("search-input");
+    const searchResults = document.getElementById("search-results");
+
+    const books = [
+        { title: "Book One", author: "Author One", isbn: "123456789" },
+        { title: "Book Two", author: "Author Two", isbn: "234567890" },
+        { title: "Book Three", author: "Author Three", isbn: "345678901" }
+    ];
+
+    addBookForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const isbn = document.getElementById("isbn").value;
+
+        if (!title || !author || !isbn) {
+            alert("Please fill out all fields to add a new book.");
+            return;
+        }
+
+        if (books.some(book => book.isbn === isbn)) {
+            alert("Book with this ISBN already exists.");
+            return;
+        }
+
+        const newBook = { title, author, isbn };
+        books.push(newBook);
+        displayBooks();
+        addBookForm.reset();
+    });
+
+    searchForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        searchBooks();
+    });
+
+    function displayBooks() {
+        bookList.innerHTML = "";
+        if (books.length === 0) {
+            bookList.textContent = "No books available.";
+            return;
+        }
+        books.forEach((book, index) => {
+            const bookItem = document.createElement("div");
+            bookItem.className = "book-item";
+            bookItem.textContent = `${book.title} by ${book.author} ISBN: ${book.isbn}`;
+            bookList.appendChild(bookItem);
+        });
+    }
+
+    function searchBooks() {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+        const filteredBooks = books.filter(book =>
+            book.title.toLowerCase().split(' ').some(word => word === searchTerm) ||
+            book.author.toLowerCase().split(' ').some(word => word === searchTerm) ||
+            book.isbn.includes(searchTerm)
+        );
+        displaySearchResults(filteredBooks);
+    }
+
+    function displaySearchResults(results) {
+        searchResults.innerHTML = "";
+        if (results.length === 0) {
+            searchResults.textContent = "No matching books found.";
+            return;
+        }
+        results.forEach(book => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${book.title} by ${book.author} ISBN: ${book.isbn}`;
+            searchResults.appendChild(listItem);
+        });
+    }
+
+    // Initial display of books
+    displayBooks();
+});
